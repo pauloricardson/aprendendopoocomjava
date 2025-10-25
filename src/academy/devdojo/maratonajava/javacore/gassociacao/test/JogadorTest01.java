f8f49a0ca856647767254297c028653c8eb40c6e
paulo <pauloricardsonsilvacosta@gmail.com>
2025-10-25 12:21:53 -0300

    Aula 64

---
+++ src/academy/devdojo/maratonajava/javacore/gassociacao/test/JogadorTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+
+    static void main() {
+        Jogador j1 = new Jogador("Neymar");
+        Jogador j2 = new Jogador("Paulinho");
+        Jogador j3 = new Jogador("Ricardinho");
+
+        Jogador[] jogadores = {j1, j2, j3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+
+    }
+}
